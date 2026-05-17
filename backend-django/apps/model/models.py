6a4b0c5bcb8b8a1e3c6d887a130ed1ecb9932439
cohermit <xieyiyangshadow@outlook.com>
2026-05-17 20:18:38 +0800

    readme

--- backend-django/apps/model/models.py
+++ backend-django/apps/model/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
+from apps.dataset.models import Dataset
 
 # Create your models here.
+
+class Model(models.Model):
